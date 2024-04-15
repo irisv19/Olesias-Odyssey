f9dd01cb25ca275d2c7fb66f24aa1809f8389851
irisv19 <143832578+irisv19@users.noreply.github.com>
2024-04-15 10:38:58 -0700

    IT WAS ALL WRONG

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-function displayText(a) {
-  console.log("test")
-  document.getElementById(a).classList.toggle('gn');
-  }
+// function displayText(a) {
+//   console.log("test")
+//   document.getElementById(a).classList.toggle('gn');
+//   }
+
+const story = {
+  start: {
+    text: "To Olessia's right there was a paved path that split into two.",
+    choices: [
+      ["section0", "Take the left path which awaits a compass and a map."],
+      ["section1", "Take the right path which awaits a satyr."]
+    ]
+  },
+}
