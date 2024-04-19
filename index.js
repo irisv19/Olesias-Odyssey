c8829bb61ffda8f0ba2ba00f946a72a212b3c0f7
irisv19 <143832578+irisv19@users.noreply.github.com>
2024-04-19 10:43:04 -0700

    We did work..so proud!!

--- index.js
+++ index.js
@@ -1,34 +1,218 @@
 const story = {
   start: {
-    text: "To the right of where she is standing there is a paved path leading alongside the forest split into two. ",
+    text: "She was stranded on a boat and washed up on shore of an island. She sickly stumbled her way around trying to catch her balance. Her stomach ached from the lack of food and hydration. She was not the only one on that boat but she was the only one ALIVE. People of the village were supposed to go on a voyage to find new land to migrate to get away from the disease that is taking over their village.",
     choices: [
-      ["example1", "The left path awaited for her a map and compass."],
-      ["end", "The right path awaits a satyr."]
+      ["example1", "The paved path"],
+      ["example2", "The forest"],
+      ["example3", "The shed"]
     ]
   },
   example1: {
-    text: "your story text here part 1",
+    text: "The paved path splits into two. The left path awaited a map and compass and the right path awaited a Satyr. Which path should Olessia take?",
     choices: [
-      ["example2", "Go to example 2"],
-      ["end", "go to the end"]
+      ["example4", "Left Path"],
+      ["example5", "Right Path"]
     ]
   },
-  example2: {
-    text: "your story text here part 2",
+
+  example4: {
+    text: "Olessia picks up the map and the compass. The map is covered in x marks so she follows it to the one closest to her. An image of a ginormous table of food showed up on the map. As she got closer she saw the table of food. She reached the table but then was caught off guard by a booby trap in the ground. She fell into a dungeon!!! What should she do now? ",
     choices: [
-      ["example1", "go to example1"],
-      ["end", "go to the end"]
+      ["example6","Drink the potion"],
+      ["example7","Call for help"]
     ]
   },
+
+  example5: {
+    text: "The right path took her to a Satyr. The Satyr introduced himself as Ajax and he began to lead her down the path. She followed along groggily and then he picked up a bag behind a bush and handed it to her. 'You're gonna need this.' he tells her. She opens the bag revealing a sword. She told him she doesn't want the sword and she just wants to go back home. What should she do now? ",
+    choices: [
+      ["example8","Take the sword"],
+      ["example9","Go back"]
+    ]
+  },
+  
+  example6: {
+    text: "When she got up there was a table with one singular purple potion. At this point Olessia is overly dehydrated and didn't even give a second thought to drinking it. She takes a big chug of the potion and her body starts to feel oddly loose. Her body began to turn into goo. What should she do now?",
+    choices: [
+      ["example10","Escape through cracks"],
+      ["example11","Explore dungeon for materials"]
+    ]
+  },
+
+  example7: {
+    text: "Olessia calls out screaming for help. Her weak, body unable to carry itself after the fall. She used her last ounce of strength to yell as loud as she can. All of sudden, appearing out of the dark, was a man? An animal? She couldn't tell. He emerged out of the mist and it seemed to be some sort of goat man. He introduced himself as Ajax and he tells her to drink the potion on the table. Next to the potion was a map. What should she do now?",
+    choices: [
+      ["example12","Do you drink potion?"],
+      ["example13","Use the map"]
+    ]
+  },
+
+  example8: {
+    text: "She decided this must mean dangerous things may happen to her on this unknown island with a talking goat man. She takes the sword and keeps it in the bag. All of a sudden they hear a rustle up ahead and Ajax immediatley bolted away. What should she do now?",
+    choices: [
+      ["example14","Run after him"],
+      ["example15","Unsheath her sword"]
+    ]
+  },
+
+  example9: {
+    text: "She decides to deny Ajax's request to take the sword and she goes back to boat in hopes to try and fix it herself. She arived at the boat to see rabie filled werewolves knawing at the dead bodies left on the boat. Horrified, she immediatley went back to find Ajax. She found him in the exact spot she left and a smile came on his face. She told him, 'Okay lets go' but she didn't take the sword. He leads her to a house filled with food. What should she do now?",
+    choices: [
+      ["example16","Eat food"],
+      ["example17","Remain cautious"]
+    ]
+  },
+
+  example10: {
+    text: "Her slimey self squeezed its way through a crack in the dungeon and found herself back going through and underground tunnel small enough for a rat. She saw a light at the end and went up and found herself at the base of a mountain. What should she do now?",
+    choices: [
+      ["example18","Climb the mountain"],
+      ["example19","Go find the path"]
+    ]
+  },
+
+  example11: {
+    text: "Her slimey body was able to easily slide past any bar, door, or crack within the dungeon. She figured she can go look for weapons or anything that can help her on this island. She came across a door that was marked as 'armed' so she decided that was her best hope. She squeezed under and was surrounded by stick and twigs. She found no use in these supplies and continued going through the dungeon. She came to the end and saw the exit but then right before it, she found a room called 'weapons' and just as she was about to go under the door she hearda thud and a loud 'HEY'. What should she do now?",
+    choices: [
+      ["example20","Hide in the room"],
+      ["example21","Escape"]
+    ]
+  },
+
+  example12: {
+    text: "The potion turned her into a puddle of goo.Her slimey self squeezed its way through a crack in the dungeon and found herself back going through and underground tunnel small enough for a rat. She saw a light at the end and went up and found herself at the base of a mountain. What should she do now?",
+    choices: [
+      ["example","Climb the mountain"],
+      ["example","Go find the path."]
+    ]
+  },
+
+  example13: {
+    text: "Olessia reached for the map and accidentally knocked over the potion. All of a sudden the stone table just melted and she quickly got a hold of the map. 'Good thing I didn't drink that' she thought to herself. She looked at the map and it showed her an escape route out the dungeon. As she walked toward the first exit all the bars in the dungeon enclosed around her and she was trapped. Forever. ",
+    choices: [
+      ["end","She died"],
+    ]
+  },
+
+  example14: {
+    text: "She followed the footsteps Ajax had left behind him and she found him hiding behind a huge boulder. He was scared and out of breath. He snatched the sword bag from her and took it out. He handed it to her and in a hushed tone he said, 'you're going to need to hold this and keep it out just in case.' After that he didn't seem to move and then he died. What should she do now?",
+    choices: [
+      ["example","Go see what made noise"],
+      ["example","Run farther"]
+    ]
+  },
+
+  example15: {
+    text: "",
+    choices: [
+      ["example",""],
+      ["example",""]
+    ]
+  },
+
+  // example: {
+  //   text: "",
+  //   choices: [
+  //     ["example",""],
+  //     ["example",""]
+  //   ]
+  // },
+   //above is story one
+  
+   example2: {
+    text: "In front of her grows a plethora of exotic looking trees and a pair of ice blue eyes staring back at her. What should she do now? ",
+    choices: [
+      ["example101", "Follow the eyes"],
+      ["example102", "Avoid the eyes"]
+    ]
+  },
+
+  example101: {
+    text: "Olesia thought maybe someone was there to help. She walked towards it and it seemed to be some sort of animal hiding behind a bush. She went for a closer look and saw it was an old white Siberian tiger named Arkin. Arkin steps out as Olesia reluctantly stands still. Arkin and Olesia stand facing each other when all of a sudden Arkin starts gagging and choking. A big mound of fur slides out of her throat and she sits there licking her paw and cleaning herself. Olesia, slightly grossed out, stands and watches at Arkin grooms herself. Arkin notices Olesia didn’t do anything so she gets up nudging the furball towards Olesia as if she was offering it for her possession. What shoulkd she do now?",
+    choices: [
+      ["example103","Should she take it"],
+      ["example104","Walk away slowly"]
+    ]
+  },
+  
+  example102: {
+    text: "She decides to avoid them, after all she doesn't know where she is and what this place is like. She's so tired she doesn't know what to do. She doesn't see a single person in sight. She sees that the boat is in terrible condition and she doesn't have the tools or equipment to fix it. What should she do now? ",
+    choices: [
+      ["example105","Look for supplies"],
+      ["example106","Try to find people"]
+    ]
+  },
+  
+  example103: {
+    text: "Olesia picks up the slimy furball with her hands and opens it up. Inside there was a bottle with a letter in it. She dries off the bottle and takes out the paper. It read, 'In lands afar, where dragons sleep, And mountains tall, their vigils keep, there lies a quest, both bold and grand, for heroes brace, to take a stand. Through forests deep, where shadows loom, and rivers rush, with whispered tunes, the chosen tune, with hearts aflame, shall seek the path to fortune's claim. With sword in hand and armor bright, they’ll face the darkness, shun the night, for treasures rare, and secrets old, await the brave, the daring bold. Through trials dire, they’ll journey on, with faith and hope, their strength not gone, for destiny calls, with voice profound, to seek the truth, where it is found. So heed the call, oh valiant souls, let courage guide, and fear control, for in this quest, lies destiny's key, to shape this world, and set it free.' What should she do now?",
+    choices: [
+      ["example107","Accept request"],
+      ["example108","Deny request"]
+    ]
+  },
+  
+  example104: {
+    text: "Caught off guard she decodes to ",
+    choices: [
+      ["example109",""],
+      ["example110",""]
+    ]
+  },
+
+  // example105: {
+  //   text: "",
+  //   choices: [
+  //     ["example111",""],
+  //     ["example112",""]
+  //   ]
+  // },
+  // example106: {
+  //   text: "",
+  //   choices: [
+  //     ["example113",""],
+  //     ["example114",""]
+  //   ]
+  // },
+  
+  example107: {
+    text: "Olesia decided to follow the quest. She didn’t know where to start. She reread the quest over and over and finally decided to ask Arkin what to do. He sat there listening and then spun around and laid down to sleep. She decided to just start walking through the forest towards the mountains that sat behind the forest. She said goodbye to Arkin but as soon as she started walking he got up and followed her but whenever she turned around to speak to him again he did the same thing and fell asleep. She gave up trying to talk to him and continued on her Journey although she felt weak and feeble. After walking over a mile she came across a stream. She drank the water and laid under a tree. The sun was slowly starting to set. The glowing red sun illuminated objects.  It was the hour of the shadows. As a kid Olesia always went outside to sit and enjoy nature and she was given hope that her journey will soon bring her great things. What should she do now?",
+    choices: [
+      ["example115","Find food"],
+      ["example116","Look for shelter & rest"]
+    ]
+  },
+  
+
+  //above is story two
+  
+  example3: {
+    text: "In front of the forest stands a beat up shed. Olesia sees footprints leading out of the shed and around the other side of the forest. Waht should she do now?",
+    choices: [
+      ["example200", "Should she go inside"],
+      ["end", "Follow steps around it"]
+    ]
+  },
+  //above is story three
+  
   end: {
     text: ["This is the end"]
   }
   // Above code is the body of the code, they are the containers that hold the text, and the corresponding choices
 };
+
+
+
+
+
+
+
+
+
+
 let storyContainer = document.getElementById("story");
 
 let buttonContainer = document.createElement("buttons");
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 4; i++) {
   buttonContainer.innerHTML = story.start.text;
   document.body.appendChild(buttonContainer);
 }
